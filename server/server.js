793e033fb58a00071802fc3955d6f181918352fd
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-03-19 15:07:19 +0200

    Add POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { mongoose } = require('./db/mogoose');
+const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/todo');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
 
 const PORT = process.env.PORT || 5000;
 

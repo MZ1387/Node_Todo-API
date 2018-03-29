d9bef1624a77adba5a7f05b2204ed561da09fa43
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-03-29 10:31:48 +0200

    Hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
-    id: 10
-}
+const password = '123abc!';
 
-const token = jwt.sign(data, '123abc');
-console.log('token', token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log('#', salt);
+//     })
+// })
 
-const decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+const hashedPassword = '$2a$10$VJBtcrhDR17/9Kxf6ZXGYe';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log('###', res);
+})
+
+// const data = {
+//     id: 10
+// }
+//
+// const token = jwt.sign(data, '123abc');
+// console.log('token', token);
+//
+// const decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // let message = 'I am user 13';
 // let hash = SHA256(message).toString();
